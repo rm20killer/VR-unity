02c66d0aac01df89e3d0c9075dc3dca21aaf55cb
RM20 <43253883+rm20killer@users.noreply.github.com>
2024-01-25 15:31:56 +0000

    lab2

---
+++ Game Platforms/Support/IMAT3002_Lab5Files/HideAfterDelay - Copy.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class HideAfterDelay : MonoBehaviour
+{
+    public float delayInSeconds = 5f;
+    public float fadeRate = 0.25f;
+
+    private CanvasGroup canvaGroup;
+    private float startTimer;
+    private float fadoutTimer;
+
+    void OnEnable()
+    {
+        canvaGroup = GetComponent<CanvasGroup>();
+        canvaGroup.alpha = 1f;
+
+        startTimer = Time.time + delayInSeconds;
+        fadoutTimer = fadeRate;
+    }
+
+    void Update()
+    {
+        // time to fade out?
+        if (Time.time >= startTimer)
+        {
+            fadoutTimer -= Time.deltaTime;
+
+            // fade out complete?
+            if (fadoutTimer <= 0)
+            {
+                gameObject.SetActive(false);
+            }
+            else
+            {
+                // reduce the alpha value
+                canvaGroup.alpha = fadoutTimer / fadeRate;
+            }
+
+            //canvaGroup.
+            //    DOFade(0f, fadeRate).
+            //    OnComplete(() => { gameObject.SetActive(false); });
+        }
+    }
+}
---
+++ Game Platforms/Support/IMAT3002_Lab6Files/GlideLocomotion.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class GlideLocomotion : LocomotionProvider
+{
+    public Transform rigRoot;
+    public Transform trackedTransform; // camera or controller, null for thumbstick
+
+    public float velocity = 2f; // meters per second
+    public float rotationSpeed = 100f; // degrees per second
+
+    private bool isMoving;
+
+    private void Start()
+    {
+        if (rigRoot == null)
+            rigRoot = transform;
+    }
+
+    private void Update()
+    {
+        if (!isMoving && !CanBeginLocomotion())
+            return;
+
+        float forward = Input.GetAxis("XRI_Right_Primary2DAxis_Vertical");
+        float sideways = Input.GetAxis("XRI_Right_Primary2DAxis_Horizontal");
+        if (forward == 0f && sideways == 0f)
+        {
+            isMoving = false;
+            EndLocomotion();
+            return;
+        }
+
+        if (!isMoving)
+        {
+            isMoving = true;
+            BeginLocomotion();
+        }
+
+        if (forward != 0f)
+        {
+            Vector3 moveDirection = Vector3.forward;
+            if (trackedTransform != null)
+            {
+                moveDirection = trackedTransform.forward;
+                moveDirection.y = 0f;
+            }
+
+            moveDirection *= -forward * velocity * Time.deltaTime;
+            rigRoot.Translate(moveDirection);
+        }
+
+        if (trackedTransform == null && sideways != 0f)
+        {
+            float rotation = sideways * rotationSpeed * Time.deltaTime;
+            rigRoot.Rotate(0, rotation, 0);
+        }
+    }
+
+
+
+    //public Transform trackedTransform; // camera or controller, null for thumbstick
+    //public Transform rigRoot;
+    //public float velocity = 2f; // meters per second
+    //public float rotationSpeed = 100f; // degrees per second
+
+    //private void Start()
+    //{
+    //    if (rigRoot == null)
+    //        rigRoot = transform;
+    //}
+
+    //private void Update()
+    //{
+    //    float vertical = Input.GetAxis("XRI_Right_Primary2DAxis_Vertical");
+    //    if (vertical != 0f)
+    //    {
+    //        Vector3 moveDirection = Vector3.forward; // trackedTransform.forward;
+    //        moveDirection.y = 0;
+    //        moveDirection *= -vertical * velocity * Time.deltaTime;
+    //        rigRoot.Translate(moveDirection);
+    //    }
+
+    //    float horizontal = Input.GetAxis("XRI_Right_Primary2DAxis_Horizontal");
+    //    if (horizontal != 0f)
+    //    {
+    //        float rotation = horizontal * rotationSpeed * Time.deltaTime;
+    //        rigRoot.Rotate(0, rotation, 0);
+    //    }
+    //}
+}
